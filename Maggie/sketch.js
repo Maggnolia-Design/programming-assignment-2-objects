844b60620784cb13d7f84b0fc954a83c0f699071
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-31 23:24:27 -0500

    init commit

---
+++ Maggie/sketch.js
@@ -0,0 +1,27 @@
+var sandwich= {
+  bread:"multigrain",
+  sauce:"guacamole",
+  meat:"roast beaf",
+  isYummy:true,
+}
+
+var Maggie = {
+  age:19,
+  heightInches:62,
+  horoscope:"aries",
+  favTeacher:"Franscisco",
+  favFood:sandwich,
+  favAnimal:"frog",
+  favPhrase:"ayy lmao",
+}
+
+console.log(Maggie.favFood)
+
+var anArray=["hello",1,"from",2,"the",3,"other",4,"side"]
+for(var i=0;i<anArray.length; i++){
+  anArray[0]
+  anArray.push[5]
+  anArray[0]="pizza"
+}
+
+console.log(anArray)
